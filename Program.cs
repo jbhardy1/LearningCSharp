5bbe98dd7937ffeed8c1085238384371b501926c
jbhardy1 <joshuablakehardy44@gmail.com>
2025-01-22 13:26:41 -0500

    Started using decision logic

--- Program.cs
+++ Program.cs
@@ -2,19 +2,34 @@
 
 
 Random dice = new Random();
-int roll = dice.Next(1, 7);
-int target = 2;
-while(roll != target){
-    if(roll > target){
-        Console.WriteLine($"Too high! {roll}");
-        roll = dice.Next(1,7);
-    }
-    else if(roll < target){
-        Console.WriteLine($"Too low! {roll}");
-        roll = dice.Next(1,7);
-    }
-    else {
-        Console.WriteLine($"Right on the money! {roll}");
-    }
-    
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+    Console.WriteLine($"New total: {total}");
+}
+
+if((roll1 == roll2) && (roll2 == roll3)){
+    Console.WriteLine("You rolled triples! +6 bonus to total");
+    total += 6;
+    Console.WriteLine($"New total: {total}");
+}
+
+if (total >= 15)
+{
+    Console.WriteLine("You win!");
+}
+
+if (total < 15)
+{
+    Console.WriteLine("Sorry, you lose.");
 }
--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64cc5c047d553d6d4022d932c4135849847e699b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f84fc2bef3f4af106f53c324d02cb1021163fde7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
