37365b0f849359e6a31e289009abab90d47bc73d
jbhardy1 <joshuablakehardy44@gmail.com>
2025-01-22 13:49:11 -0500

    completed logic modules

--- Program.cs
+++ Program.cs
@@ -1,35 +1,64 @@
-﻿Console.WriteLine("Hello, C#!"); //initial line from project creation
+﻿// Console.WriteLine("Hello, C#!"); //initial line from project creation
 
 
-Random dice = new Random();
+// Random dice = new Random();
 
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
 
-int total = roll1 + roll2 + roll3;
+// int total = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
-{
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    total += 2;
-    Console.WriteLine($"New total: {total}");
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)){
+
+//     if((roll1 == roll2) && (roll2 == roll3)){
+//         total += 6;
+//         Console.WriteLine($"You rolled triples! +6 bonus to total! \nNew Total: {total}");
+//     }
+//     else{
+//         total += 2;
+//         Console.WriteLine($"You rolled doubles! +2 bonus to total! \nNew total: {total}");
+//     }
+// }
+
+// if (total >= 16)
+// {
+//     Console.WriteLine("You win a new car!");
+// }
+
+// else if(total >= 10){
+//     Console.WriteLine("You win a new Laptop!");
+// }
+
+// else if(total == 7){
+//     Console.WriteLine("You win a trip for two!");
+// }
+// else
+// {
+//     Console.WriteLine("Sorry, you lose.");
+// }
+
+Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration <= 10  && daysUntilExpiration > 0){
+    Console.WriteLine("Your subscription expires soon. Renew now!");
+    Console.WriteLine(daysUntilExpiration);
+
+    if(daysUntilExpiration <= 5 && daysUntilExpiration > 1){
+        discountPercentage = 10;
+        Console.WriteLine($"Renew now and save {discountPercentage}%");
+    }
+
+    else if(daysUntilExpiration == 1){
+        discountPercentage = 20;
+        Console.WriteLine($"Renew now and save {discountPercentage}%");
+    }
 }
 
-if((roll1 == roll2) && (roll2 == roll3)){
-    Console.WriteLine("You rolled triples! +6 bonus to total");
-    total += 6;
-    Console.WriteLine($"New total: {total}");
-}
-
-if (total >= 15)
-{
-    Console.WriteLine("You win!");
-}
-
-if (total < 15)
-{
-    Console.WriteLine("Sorry, you lose.");
+else if (daysUntilExpiration == 0) {
+    Console.WriteLine("Your subscription has expired");
 }
--- obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f84fc2bef3f4af106f53c324d02cb1021163fde7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bbe98dd7937ffeed8c1085238384371b501926c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
