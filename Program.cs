f84fc2bef3f4af106f53c324d02cb1021163fde7
jbhardy1 <joshuablakehardy44@gmail.com>
2025-01-22 13:01:52 -0500

    finished unit 3 of methods

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+﻿Console.WriteLine("Hello, C#!"); //initial line from project creation
+
+
+Random dice = new Random();
+int roll = dice.Next(1, 7);
+int target = 2;
+while(roll != target){
+    if(roll > target){
+        Console.WriteLine($"Too high! {roll}");
+        roll = dice.Next(1,7);
+    }
+    else if(roll < target){
+        Console.WriteLine($"Too low! {roll}");
+        roll = dice.Next(1,7);
+    }
+    else {
+        Console.WriteLine($"Right on the money! {roll}");
+    }
+    
+}
